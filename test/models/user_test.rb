e6e8edca2c049b5da851338b537ec08ebdd5f070
zeljkoveic <zeljko@thehyperactive.com>
2014-08-07 14:55:12 +0200

    created tests for registering new user, added validation to profile_name (uniqueness, formatting)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -30,5 +30,28 @@ class UserTest < ActiveSupport::TestCase
 
 	end
 
+	test "a user should have unique profile name" do
+  		user = User.new
+  		
+  		user.profile_name = users(:zeljko).profile_name
+
+  		assert !user.save
+  		puts user.errors.inspect
+  		assert !user.errors[:profile_name].empty?
+
+	end
+
+	test "a user should have a  profile name without spaces" do
+
+		user = User.new
+		user.profile_name = "My profile name"
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+		assert user.errors[:profile_name].include?("Must be formatted correctly")
+
+	end
+
+
 
 end
