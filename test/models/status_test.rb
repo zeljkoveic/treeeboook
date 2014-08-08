fffe66043dfe7d9024c02f3eff08cac567a5b68e
zeljkoveic <zeljko@thehyperactive.com>
2014-08-08 15:38:46 +0200

    added status validations and tests

--- test/models/status_test.rb
+++ test/models/status_test.rb
@@ -4,4 +4,25 @@ class StatusTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "that a status requires content" do
+  	status = Status.new
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+  test "that a status's content is at least 2 letters long" do
+  	status = Status.new
+  	status.content = 'H'
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+  test "that a status has a user id" do
+  	status = Status.new
+  	status.content = "hello"
+  	assert !status.save
+  	assert !status.errors[:user_id].empty?
+  end
+
 end
