ebf7318778cc1a55200bcccdaeecbfe70a9e92a6
zeljkoveic <zeljko@thehyperactive.com>
2014-08-08 13:53:28 +0200

    added custom routes for  authentication and feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'sign_in', to: 'devise/sessions#new', as: :sign_in
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+
   resources :statuses
+
+  get 'feed', to: 'statuses#index', as: :feed
 
   root to: 'statuses#index'
 
