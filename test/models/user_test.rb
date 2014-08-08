839af1a51c6a7c489733e979db5db1507ccdca42
zeljkoveic <zeljko@thehyperactive.com>
2014-08-08 15:54:21 +0200

    created tests to fix (already fixed) issue with regex

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -43,14 +43,24 @@ class UserTest < ActiveSupport::TestCase
 
 	test "a user should have a  profile name without spaces" do
 
-		user = User.new
-		user.profile_name = "My profile name"
+		user = User.new(first_name: 'jason', last_name: 'seifer', email: "hello@example.com")
+    user.password = user.password_confirmation = 'dssdfsfs'
+
+    user.profile_name = "jasonseiferr rrrr1212_ds-22"
 
 		assert !user.save
 		assert !user.errors[:profile_name].empty?
 		assert user.errors[:profile_name].include?("Must be formatted correctly")
 
 	end
+
+  test "user has a correct formatted profile name" do
+    user = User.new(first_name: 'jason', last_name: 'seifer', email: "hello@example.com")
+    user.password = user.password_confirmation = 'dssdfsfs'
+
+    user.profile_name = "jasonseiferrrrrr1212_ds-22"
+    assert user.valid?
+  end
 
 
 
